532abf5d1c46a90d6bf519a3486f9262aee4d094
anhtuanzxc710-a11y <anhtuanzxc710@gmail.com>
2026-05-13 01:23:43 +0700

    UPDATE CONTACT
    SỬA ĐỊA CHỈ

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -3,7 +3,7 @@ import { assets } from '../assets/assets'
 import { useNavigate } from 'react-router-dom'
 
 const Contact = () => {
-  const navigate=useNavigate();
+  const navigate = useNavigate();
   return (
     <div>
       {/*left is image*/}
@@ -11,21 +11,21 @@ const Contact = () => {
         <b>CONTACT US</b>
       </div>
       <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28 text-sm'>
-      <div>
-        <img className='w-full md:max-w-[480px]' src={assets.contact_us} alt=""/>
-      </div>
-      <div  className='flex flex-col justify-center items-start gap-6'>
-        <p  className='flex flex-col justify-center font-semibold'>Our OFFICE</p>
-        <p className='text-gray-600'>1 Dai Co Viet, Hai Ba Trung<br/> 
-        Hanoi, Vietnam</p>
-        <p className='text-gray-600'>Tel: (84) 862613118<br/>Email: <a href="mailto:web262004@gmail.com" className="text-primary hover:underline">web2620042004@gmail.com</a></p>
-        <p  className='flex flex-col justify-center font-semibold'>Careers at our shop</p>
-        <p className='text-gray-600'>Learn more about our teams and job openings.</p>
-        <button onClick={()=>navigate('/jobs')} className="bg-gray-200 text-gray-800 border-1 px-4 py-2 rounded transition-transform duration-300 hover:bg-primary hover:text-white hover:scale-110">
-  Explore Jobs
-</button>
+        <div>
+          <img className='w-full md:max-w-[480px]' src={assets.contact_us} alt="" />
+        </div>
+        <div className='flex flex-col justify-center items-start gap-6'>
+          <p className='flex flex-col justify-center font-semibold'>Our OFFICE</p>
+          <p className='text-gray-600'>10 TRẦN PHÚ HÀ ĐÔNG<br />
+            Hanoi, Vietnam</p>
+          <p className='text-gray-600'>Tel: (84) 862613118<br />Email: <a href="mailto:web262004@gmail.com" className="text-primary hover:underline">web2620042004@gmail.com</a></p>
+          <p className='flex flex-col justify-center font-semibold'>Careers at our shop</p>
+          <p className='text-gray-600'>Learn more about our teams and job openings.</p>
+          <button onClick={() => navigate('/jobs')} className="bg-gray-200 text-gray-800 border-1 px-4 py-2 rounded transition-transform duration-300 hover:bg-primary hover:text-white hover:scale-110">
+            Explore Jobs
+          </button>
 
-      </div>
+        </div>
       </div>
     </div>
   )
